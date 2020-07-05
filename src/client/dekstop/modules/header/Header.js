6054ed6948e824c6d8346dc35f4b3d79473285e0
Nikolas9898 <nikola@3web-studio.com>
2020-07-05 23:01:11 +0300

    added chages

---
+++ src/client/dekstop/modules/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = props => {
+    return (
+        <div>
+            This is the header
+        </div>
+    );
+};
+
+Header.propTypes = {
+
+};
+
+export default Header;
