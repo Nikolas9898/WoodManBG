3cc6f2acded50769006acc7e90ab4f468809d62b
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2020-07-05 22:56:02 +0300

    creating blog

---
+++ src/client/admin/modules/blog/BlogContainer.js
@@ -0,0 +1,23 @@
+import React from "react"
+class BlogContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+BlogContainer.propTypes = {
+
+};
+
+export default BlogContainer;
