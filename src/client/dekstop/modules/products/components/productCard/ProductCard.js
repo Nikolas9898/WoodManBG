178eb8b35f5164108f4fd8ed4d5e4962b454bd19
Nikolas9898 <nikola@3web-studio.com>
2020-07-31 21:52:55 +0300

    adding changes

--- src/client/dekstop/modules/products/components/productCard/ProductCard.js
+++ src/client/dekstop/modules/products/components/productCard/ProductCard.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ProductCardStyle from './ProductCard.module.css';
-import {Link} from "react-router-dom";
-import Datetime from 'react-datetime';
+import React from "react";
+import PropTypes from "prop-types";
+import ProductCardStyle from "./ProductCard.module.css";
+import { Link } from "react-router-dom";
+import Datetime from "react-datetime";
 import moment from "moment";
-const ProductCard = ({product}) => {
-    return (
-        <div className={ProductCardStyle.card_holder}>
-            <img src={product.image} className={ProductCardStyle.card_image}/>
+const ProductCard = ({ product }) => {
+  return (
+    <div className={ProductCardStyle.card_holder}>
 
-            <h1 className={ProductCardStyle.card_title}>
-                {product.title}
-            </h1>
-            <div className={ProductCardStyle.date}>
-                Добавен на:{" "}
-                {moment(product.updateAt).format('DD/MM/YYYY')}
-            </div>
-        </div>
-    );
+      <img
+        src={product.mainImagePath}
+        className={ProductCardStyle.card_image}
+      />
+
+      <h1 className={ProductCardStyle.card_title}>{product.title}</h1>
+      <div className={ProductCardStyle.date}>
+        Добавен на: {moment(product.updateAt).format("DD/MM/YYYY")}
+      </div>
+    </div>
+  );
 };
 
-ProductCard.propTypes = {
-
-};
+ProductCard.propTypes = {};
 
 export default ProductCard;
