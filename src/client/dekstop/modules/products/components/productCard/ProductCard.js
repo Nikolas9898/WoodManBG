ebeeffaeae543e76f04bbb9e1c815242e1813c89
georgi.marcenkov <georgi.marcenkov@3web-studio.com>
2020-08-02 21:15:24 +0300

    new

--- src/client/dekstop/modules/products/components/productCard/ProductCard.js
+++ src/client/dekstop/modules/products/components/productCard/ProductCard.js
@@ -2,18 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import ProductCardStyle from "./ProductCard.module.css";
 import { Link } from "react-router-dom";
-import Datetime from "react-datetime";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faTag} from "@fortawesome/free-solid-svg-icons";
 import moment from "moment";
+
 const ProductCard = ({ product }) => {
   return (
     <div className={ProductCardStyle.card_holder}>
+      {console.log(product)}
       <img
         src={product.mainImagePath}
         className={ProductCardStyle.card_image}
       />
       <h1 className={ProductCardStyle.card_title}>{product.title}</h1>
+      <div className={ProductCardStyle.category}>
+          <FontAwesomeIcon icon={faTag} />
+        Кухня
+      </div>
       <div className={ProductCardStyle.date}>
-        Добавен на: {moment(product.updateAt).format("DD/MM/YYYY")}
+        Добавен: {moment(product.updateAt).format("DD/MM/YYYY")}
       </div>
     </div>
   );
